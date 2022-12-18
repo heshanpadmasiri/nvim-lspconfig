d1ea63d6cdd5e8e9b244745df59c7dfdc30ded3d
Heshan Padmasiri <hpheshan@gmail.com>
2022-12-18 12:37:18 +0530

    add args

--- lua/lspconfig/server_configurations/ballerina.lua
+++ lua/lspconfig/server_configurations/ballerina.lua
@@ -2,7 +2,7 @@ local util = require 'lspconfig.util'
 
 return {
   default_config = {
-    cmd = { 'bal' },
+    cmd = { 'bal', 'start-language-server' },
     filetypes = { 'ballerina' },
     root_dir = util.root_pattern('Ballerina.toml')
   }
